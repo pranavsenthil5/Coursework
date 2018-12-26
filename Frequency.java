b062817ece68f9c60f6b89dd59e3699de657cc9a
pranav <pranavsenthil5@gmail.com>
2018-12-26 14:48:10 +0530

    gd...untill sw came...

--- Frequency.java
+++ Frequency.java
@@ -5,31 +5,77 @@ class Frequency
     {
         Scanner s = new Scanner(System.in);
         System.out.println("Input:");
-        int input = s.nextInt();
-        int[] array = Sort(input);
-        int count=0;
+        String input = s.nextLine();
+        String [] a = input.split(","); 
+        
+        int[] array= new int[a.length];
+        for(int i=0;i<a.length;i++)
+        array[i]=Integer.parseInt(a[i]);
+        
+        array = Sort(array);
+        int count=1;
         for(int i=0;i<array.length-1;i++)
         {
             if(array[i]!=array[i+1])
-            count++;
+                count++;
         }
         int [] freq= new int[count];
         int [] value= new int[count];
         int j=0;
-        
-        for(int i=0;i<array.length-1;i++)
+
+        for(int i=0;i<array.length;i++)
         {
-            if(array[i]!=array[i+1])
+            try{
+                if(array[i]!=array[i+1])
+                {
+                    value[j]= array[i];
+                    j++;
+                }
+            }
+            catch(Exception e)
             {
                 value[j]= array[i];
+                    j++;
             }
         }
+
         for(int i=0;i<count;i++)
         {
-            
+            freq[i]=count(array,value[i]);
+            System.out.println("----------------\nN-"+value[i]+" F-"+freq[i]);
         }
-        
+        j=0;
+        for(int i=0;i<freq.length;i++)
+        {
+            for(;j<freq.length-1;j++)
+            {
+                if(freq[j]>freq[j+1])
+                {
+                    int temp=freq[j];
+                    freq[j]=freq[j+1];
+                    freq[j+1]=temp;
+                    
+                     temp=value[j];
+                    value[j]=value[j+1];
+                     value[j+1]=temp;
+                }
+            }
+            j=0;
+        }
+        int output= (count(freq,freq[0])==1) ? value[0]: value[value.length-1]; 
+        System.out.println(output);
     }
+    // }
+    // boolean exists(int []array,int value)
+    // {
+    // for(int i=0;i<array.length;i++)
+    // {
+    // if(array[i]==value)
+    // return true;
+    // }
+    // return false;
+    // }
+
     public static int count(int [] array,int value)
     {
         int count =0;
@@ -42,19 +88,13 @@ class Frequency
         }
         return count;
     }
-    public static int[] Sort(int input)
+
+    public static int[] Sort(int []array)
     {
-        String s = Integer.toString(input);
-        char[] c = s.toCharArray();
-        int [] array = new int[c.length];
-        for(int i =0;i<c.length;i++)
+
+        for(int i=0;i<array.length;i++)
         {
-            array[i]= c[i];
-        }
-        
-        for(int i=0;i<c.length;i++)
-        {
-            for(int j=0;j<c.length-1;j++)
+            for(int j=0;j<array.length-1;j++)
             {
                 if(array[j]>array[j+1])
                 {
@@ -64,7 +104,7 @@ class Frequency
                 }
             }
         }
-       
+
         return array;
     }
 }
