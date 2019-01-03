f9a5cdf5361fbcdd28b53076b99561be53c66279
pranav <pranavsenthil5@gmail.com>
2019-01-03 09:58:17 +0530

    Decode...until bhai came...

---
+++ Decode.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Decode
+{
+    public static void main(String []args)
+    {
+        String input="UHINBY LKKQCH HYLKK";
+        int Shift=7;
+        String a[]=input.split(" ");
+        input=a.toString();
+        String output="";
+        for(int i=0;i<input.length()-1;i++)
+        {
+            output=output+(((int)input.chatAt(i))-)
+        }
+        
+    }
+}
