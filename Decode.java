03dfe0cb0badd23afdf1b22b75df27a154a845db
pranav <pranavsenthil5@gmail.com>
2019-01-03 10:52:33 +0530

    Finished cryptography

--- Decode.java
+++ Decode.java
@@ -3,15 +3,71 @@ class Decode
 {
     public static void main(String []args)
     {
-        String input="UHINBY LKKQCH HYLKK";
-        int Shift=7;
-        String a[]=input.split(" ");
-        input=a.toString();
-        String output="";
-        for(int i=0;i<input.length()-1;i++)
+        Scanner s = new Scanner (System.in);
+        System.out.println("Input Line:");
+        String input=s.nextLine();
+        System.out.println("Input Shift:");
+        int Shift=s.nextInt();
+        System.out.println("___________");
+        System.out.println("1-To Encode\n2-To Decode");
+        System.out.println("Input 1 or 2:");
+        int option=s.nextInt();
+
+        if(option==2)
         {
-            output=output+(((int)input.chatAt(i))-)
+            String a[]=input.split(" ");
+            input="";
+            
+            for(int i=0;i<a.length;i++)
+            {
+                input+=a[i];
+            }
+
+            String output="";
+            for(int i=0;i<input.length();i++)
+            {
+                //System.out.println(input.charAt(i));
+                int val1=((int)input.charAt(i));
+                val1=val1-65;
+                val1+=Shift-1;
+                val1=val1%26;
+                val1+=65;
+
+                output=output+((char)val1);
+            }
+            output=output.replace("QQ"," ");
+            output=output.trim();
+            System.out.println(output);
         }
-        
+        else if(option==1)
+        {
+            input+=" ";
+            input=input.replace(" ","QQ");
+            
+            String output="";
+            for(int i=0;i<input.length();i++)
+            {
+                //System.out.println(input.charAt(i));
+                int val1=((int)input.charAt(i));
+                val1=val1-65;
+                val1=val1+(26-Shift+1);
+                val1=val1%26;
+                val1+=65;
+
+                output=output+((char)val1);
+            }
+            //System.out.println(output);
+            String output2="";
+            for(int i=0;i<output.length();i++)
+            {
+                if(i>5&&i%6==0)
+                System.out.print(" ");
+                System.out.print(output.charAt(i));
+            }
+        }
+        else
+        {
+            System.out.println("Wrong Option");
+        }
     }
 }
